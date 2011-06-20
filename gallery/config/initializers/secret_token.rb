ccdbf7a30eaaef9620ede61f9b28a339cec0b8c4
Ларичева Алена <lelja404@gmail.com>
2011-06-20 13:15:05 +0700

    Изменен routes.rb

---
+++ gallery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = '98b9f9a906993b7a86ad6182299b41551748a7ef633935e0fca9ac0daecdd47254f90dc275f740035c99dbcac9253f57390d56eee0fdd65951fbed3e968f847b'
---
+++ gallery/db/migrate/20110620044938_create_photos.rb
@@ -0,0 +1,23 @@
+class CreatePhotos < ActiveRecord::Migration
+  def self.up
+    create_table :photos do |t|
+      t.string :title
+      t.text :description
+      t.string :filename
+      t.string :content_type
+      t.integer :size
+      t.integer :width
+      t.integer :height
+      t.string :path
+      t.string :thumbnail
+      t.integer :user_id
+      t.integer :parent_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :photos
+  end
+end
