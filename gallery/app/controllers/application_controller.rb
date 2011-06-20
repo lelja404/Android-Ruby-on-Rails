b29bd11b5598c52ee978b39c79fb6392a944ff9a
Ларичева Алена <lelja404@gmail.com>
2011-06-20 18:35:11 +0700

    Сделано оформление. Не работает загрузка изображений, ну и отображений их соответственно.

--- gallery/app/controllers/application_controller.rb
+++ gallery/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout 'application'
+  def initialize
+    super
+    @current_user=User.first
+  end
 end
--- gallery/config/routes.rb
+++ gallery/config/routes.rb
@@ -63,5 +63,6 @@ Gallery::Application.routes.draw do |map|
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  map.connect '', :controller => "photos"
+  #map.connect '', :controller => "photos"
+  root :to => "photos#index"
 end
