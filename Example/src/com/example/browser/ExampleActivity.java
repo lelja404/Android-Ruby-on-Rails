ce5bd027e658b83e44b1f1877245f64c1c24700f
Ларичева Алена <lelja404@gmail.com>
2011-06-22 17:55:52 +0700

    Android XML Parsing (images)
    It works! But displays only the text

--- Example/src/com/example/browser/ExampleActivity.java
+++ Example/src/com/example/browser/ExampleActivity.java
@@ -1,28 +1,60 @@
 package com.example.browser;
 
-import android.app.Activity;
-import android.webkit.*;
-import android.os.Bundle;
+import java.net.URL;
 import com.example.browser.R;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
 
 public class ExampleActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        WebView webview = (WebView) this.findViewById(R.id.webview);
-
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
         
-        WebSettings websettings = webview.getSettings();
+        try {
+        TextView name[];
+        URL url = new URL("http://10.0.2.2:3000/photos.xml");
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new InputSource(url.openStream()));
+        doc.getDocumentElement().normalize();
+ 
+        NodeList nodeList = doc.getElementsByTagName("photo");
+        name = new TextView[nodeList.getLength()];
+        
+        for (int i = 0; i < nodeList.getLength(); i++) {
 
-        
-        websettings.setJavaScriptEnabled(true);
+        	Node node = nodeList.item(i);
 
-        
-        websettings.setBuiltInZoomControls(true);
-        webview.loadUrl("http://10.0.2.2:3000");
-        
+        	name[i] = new TextView(this);
+        	Element fstElmnt = (Element) node;
+        	NodeList nameList = fstElmnt.getElementsByTagName("image-file-name");
+        	Element nameElement = (Element) nameList.item(0);
+        	nameList = nameElement.getChildNodes();
+        	name[i].setText("Name = "
+        	+ ((Node) nameList.item(0)).getNodeValue());
+        	layout.addView(name[i]);
+        }
+    } catch (Exception e) {
+    System.out.println("XML Pasing Excpetion = " + e);
     }
+    
+
+setContentView(layout);
+}
 }
